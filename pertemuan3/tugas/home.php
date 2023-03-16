d10182458c91caa22c1ceb1d7eb6b0bbdf689a72
MuhZaidanRamdhan <muhammadzaidanramdhan509@gmail.com>
2023-03-16 09:40:40 +0700

    praktikum

--- pertemuan3/tugas/home.php
+++ pertemuan3/tugas/home.php
@@ -3,9 +3,11 @@ include_once 'header.php';
 ?>
 				<h1>Welcome Home!</h1>
 				<p>
-				    <!--
-				        Tuliskan Nama - NIM - Rombel 
-				    -->
+				    
+				        Nama = Muhammad Zaidan Ramdhan <br>
+						NIM = 01110222040 <br>
+						Rombel = TI12 
+				    
 				</p>
 <?php
     include_once 'footer.php';
